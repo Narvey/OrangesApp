31c8fb31f34f9db3d96f1786923ed2dec23dd91e
N Harvey <nthnhrvy@gmail.com>
2012-10-19 09:53:14 -0600

    sync

--- src/com/example/orange/picking/app/MainActivity.java
+++ src/com/example/orange/picking/app/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.orange.picking.app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
@@ -20,4 +23,15 @@ public class MainActivity extends Activity {
     	or.setImageDrawable(Drawable.createFromPath("res/drawable-hdpi/orangehighlight.png"));
     }
     
+    public class Orange extends ImageButton{//TODO may need to remove the extends
+    	private Bitmap orange;
+    	private int top;
+    	private int left;
+		public Orange(Context context) {
+			super(context);
+			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orange);
+			
+		}
+    	
+    }
 }
