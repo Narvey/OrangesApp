f90de115b96c2a1b74898b67ec17590030d2ad3d
N Harvey <nthnhrvy@gmail.com>
2012-10-19 15:04:15 -0600

    add some util functions

--- src/com/example/orange/picking/app/MainActivity.java
+++ src/com/example/orange/picking/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.orange.picking.app;
 
+import android.R.string;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -7,6 +8,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -18,9 +20,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
     
-    public void MoveOrange(View view){
-    	ImageButton or = (ImageButton) view;
+    @Override 
+    public boolean onTouchEvent(MotionEvent event){
+    	super.onTouchEvent(event);
+    	ImageButton or = new ImageButton(this);
     	or.setImageDrawable(Drawable.createFromPath("res/drawable-hdpi/orangehighlight.png"));
+    	return true;
     }
     
     public class Orange extends ImageButton{//TODO may need to remove the extends
@@ -31,6 +36,12 @@ public class MainActivity extends Activity {
 			super(context);
 			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orange);
 			
+		}
+		public void makeHighlighted(){
+			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orangehighlight);
+		}
+		public void makePlaced(){
+			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orangeplaced);
 		}
     	
     }
