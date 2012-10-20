df04dc5e0101c4ea3160890a8f3409403b549311
N Harvey <nthnhrvy@gmail.com>
2012-10-19 21:23:51 -0600

    ready to submit

--- gen/com/example/orange/picking/app/R.java
+++ gen/com/example/orange/picking/app/R.java
@@ -20,10 +20,11 @@ public final class R {
         public static final int orangeplaced=0x7f020006;
     }
     public static final class id {
-        public static final int framer=0x7f070000;
+        public static final int framer=0x7f070002;
         public static final int imageView1=0x7f070001;
-        public static final int imageView2=0x7f070002;
-        public static final int menu_settings=0x7f070003;
+        public static final int imageView2=0x7f070003;
+        public static final int menu_settings=0x7f070004;
+        public static final int outer=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/orange/picking/app/OrangePick.java
+++ src/com/example/orange/picking/app/OrangePick.java
@@ -1,7 +1,6 @@
 package com.example.orange.picking.app;
 
 import java.util.Random;
-
 import android.R.string;
 import android.os.Bundle;
 import android.app.Activity;
@@ -11,36 +10,50 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 
 public class OrangePick extends Activity {
-	Orange[] tree;
-	@SuppressWarnings("unused")
+	private Orange[] tree;
+	public static final int NUMORANGES=3;
+	public static final String TAG = "OrangePick";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		tree = new Orange[3];
+		tree = new Orange[NUMORANGES];
 		FrameLayout frame = (FrameLayout) findViewById(R.id.framer);
-		for(Orange each : tree) {
-			each = new Orange(this);
-			frame.addView(each);
+		for(int i = 0; i<NUMORANGES; i++) {
+			tree[i] = new Orange(this);
+			frame.addView(tree[i]);
 		}
 	}
+
 
 	@Override 
 	public boolean onTouchEvent(MotionEvent event){
 		super.onTouchEvent(event);
-		int eventaction = event.getAction();
 		for(Orange each : tree){
 			each.handleTouch(event);
 		}
 		// tell the system that we handled the event and no further processing is required
 		return true; 
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		tree = new Orange[NUMORANGES];
+		FrameLayout frame = (FrameLayout) findViewById(R.id.framer);
+		frame.removeAllViews();
+		for(int i = 0; i<NUMORANGES; i++) {
+			tree[i] = new Orange(this);
+			frame.addView(tree[i]);
+		}
 	}
 
 	public class Orange extends View{//TODO may need to remove the extends
@@ -53,22 +66,27 @@ public class OrangePick extends Activity {
 			super(context);
 			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orange);
 			Random r = new Random();
-			top = r.nextInt(60);
-			left = r.nextInt(60);
+			top = r.nextInt(200);
+			left = r.nextInt(100);
 			invalidate();
 		}
 		public void makeHighlighted(){
+			//if()
+			Log.d(TAG, "Orange has been pressed");
 			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orangehighlight);
 			moving = true;
 			invalidate();
 		}
 		public void makeNormal(){
+			Log.d(TAG, "Orange has been released");
 			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orange);
 			invalidate();
 		}
 		public void makePlaced(){
+			Log.d(TAG, "Orange has been placed in basket!");
 			orange=BitmapFactory.decodeResource(getResources(), R.drawable.orangeplaced);
 			moving = false;//should be already, but just in case.
+			done = true;
 			invalidate();
 		}
 		/**
@@ -79,7 +97,7 @@ public class OrangePick extends Activity {
 		 */
 		public boolean moveTo(int x, int y){
 			if(x<0||y<0)return false;
-			top=y;
+			top=y-72;
 			left=x;
 			invalidate();
 			return true;
@@ -88,9 +106,11 @@ public class OrangePick extends Activity {
 
 		public void handleTouch(MotionEvent event){
 			int eventaction=event.getAction();
+
 			switch (eventaction) {
 			case MotionEvent.ACTION_DOWN: 
-				if(!done){
+				if(!done&&event.getX()>=left&&event.getX()<=getR()
+				&&event.getY()-72>=top&&event.getY()-72<=getB()){
 					moving = true;
 					makeHighlighted();
 				}
@@ -98,16 +118,28 @@ public class OrangePick extends Activity {
 
 			case MotionEvent.ACTION_MOVE:
 				if(moving){
+					Log.v(TAG, "moving orange");
 					moveTo((int)event.getX(), (int)event.getY());
 				}
 				break;
 
 			case MotionEvent.ACTION_UP:   
 				// finger leaves the screen
+				if(moving)Log.v(TAG,"Orange dropped at ("+left+","+top+")");
 				moving = false;
+				if(top>510&&left>=150&&left<=275){
+					makePlaced();
+					done=true;
+				}else makeNormal();
 				break;
 			}
 
+		}
+		public int getR(){
+			return left+orange.getWidth();
+		}
+		public int getB(){
+			return top+orange.getHeight();
 		}
 
 		/**
